2099b484d8ae9e934e4611040140291a08a6d7a8
abrahymwdad0-cell <abrahymwdad0@gmail.com>
2026-09-20 19:57:15 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'veya-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET' || new URL(req.url).origin !== location.origin) return;
+  e.respondWith(caches.match(req).then(hit => hit || fetch(req)));
+});
